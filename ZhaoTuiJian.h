6ac52f26c825813671379c3ae9cf6bcda3a988ad
MrMayBee <479027322@qq.com>
2016-08-19 10:56:53 +0800

    Add files via upload

---
+++ ZhaoTuiJian.h
@@ -0,0 +1,16 @@
+//
+//  ZhaoTuiJian.h
+//  Taaza
+//
+//  Created by ZhangJC on 16/7/12.
+//  Copyright © 2016年 MayBee Walson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface ZhaoTuiJian : UIView
+
+@property (nonatomic ,assign) CGPoint startPoint;//触摸起始点
+
+@property (nonatomic ,assign) CGPoint endPoint;//触摸结束点
+
+@end
